ef41a103075e42ca0a8c909c6085e2a2a1e0cf31
Leo Vargas <leovargas@localhost.localdomain>
2018-03-22 10:41:07 -0300

    quase funfando

--- sixflags.java
+++ sixflags.java
@@ -2,57 +2,41 @@
 import java.util.Scanner;
 import java.io.IOException;
 class Main{
-	public static int n; // numero de brinquedos
-	public static int t; // tempo limite no parque
+	public static int n = 1; // numero de brinquedos
+	public static int t = 1	; // tempo limite no parque
 	public static int[] duracao;
 	public static int[] pontuacao;
 
 	public static int colecao(int a, int tempo){
-		if (a >= n || tempo < 0){
-
-			for(int i = 0; i <= a+1; i++) System.out.print("\t");
-			System.out.println("return: " + -1123410);
-
+		int j = 0;
+		if (a >= n || tempo < 0)
 			return -12345647;
-		}
-		if (tempo == 0){
+		if (tempo == 0)
 			return 0;
-		}
-		for(int i = 0; i <= a+1; i++) System.out.print("\t");
-		System.out.println("j = " + pontuacao[a] + " + colecao(" + a + ", " + (tempo-duracao[a]) + ")");
-
-		int j = pontuacao[a] + colecao(a, tempo-duracao[a]);
-
-		for(int i = 0; i <= a+1; i++) System.out.print("\t");
-		System.out.println("y = colecao(" + (a+1) + ", " + tempo + ")");
-
+		if(tempo >= duracao[a])
+			j = pontuacao[a] + colecao(a, tempo-duracao[a]);
 		int y = colecao(a+1, tempo);
-		if(j >= y){
-
-			for(int i = 0; i <= a+1; i++) System.out.print("\t");
-			System.out.println("return(j): " + j);
-
+		if(j >= y)
 			return j;
-		}
-
-		for(int i = 0; i <= a+1; i++) System.out.print("\t");
-		System.out.println("return(y): " + y);
-
 		return y;
 	}
 
 	public static void main( String [] args ) throws IOException {
 		Scanner scanner = new Scanner(System.in);
-		n = scanner.nextInt(); // numero de brinquedos
-	    t = scanner.nextInt(); // tempo limite no parque
-		duracao = new int[n];
-		pontuacao = new int[n];
-
-		for(int w = 0; w < n; w++){
-			duracao[w] = scanner.nextInt(); // duração brinquedo
-			pontuacao[w] = scanner.nextInt(); // pontuação brinquedo
-		}
-		int result = colecao(0, t);
-		System.out.println("result: " + result);
+		int r = 0;
+		do{
+			r++;
+			n = scanner.nextInt(); // numero de brinquedos
+		    t = scanner.nextInt(); // tempo limite no parque
+			if(n == 0 && t == 0) break;
+			duracao = new int[n];
+			pontuacao = new int[n];
+			for(int w = 0; w < n; w++){
+				duracao[w] = scanner.nextInt(); // duração brinquedo
+				pontuacao[w] = scanner.nextInt(); // pontuação brinquedo
+			}
+			System.out.println("Instancia " + r);
+			System.out.println(colecao(0, t));
+		}while(true);
 	}
 }
