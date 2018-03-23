8f48224be0999c347cfa21199eabf21ed184438c
LeeonardoVargas <leu1607@hotmail.com>
2018-03-22 21:13:09 -0300

    problema 1487 funcionando

--- sixflags.java
+++ sixflags.java
@@ -6,18 +6,26 @@ class Main{
 	public static int t; // tempo limite no parque
 	public static int[] duracao;
 	public static int[] pontuacao;
+	public static int[][] aux;
 
 	public static int colecao(int a, int tempo){
-		int j = 0, y = 0;
+		int j = 0, y = 0, p = (tempo-1);
 		if (a >= n || tempo < 0)
 			return -12345647;
 		if (tempo == 0)
 			return 0;
-		if(tempo >= duracao[a] && pontuacao[a] > 0)
-			j = pontuacao[a] + colecao(a, tempo-duracao[a]);
+		if(tempo >= duracao[a] && pontuacao[a] > 0){
+			if(aux[a][p] == -1)
+				j = pontuacao[a] + colecao(a, tempo-duracao[a]);
+			else
+				j = aux[a][p];
+		}
 		y = colecao(a+1, tempo);
-		if(j >= y)
+		if(j >= y){
+			aux[a][p] = j;
 			return j;
+		}
+		aux[a][p] = y;
 		return y;
 	}
 	public static void main( String [] args ) throws IOException {
@@ -26,16 +34,18 @@ class Main{
 		do{
 			n = scanner.nextInt(); // numero de brinquedos
 		    t = scanner.nextInt(); // tempo limite no parque
-			if(n == 0 && t == 0)
-				break;
+			if(n == 0 && t == 0) break;
 			duracao = new int[n];
 			pontuacao = new int[n];
+			aux = new int[n][t];
 			for(int w = 0; w < n; w++){
 				duracao[w] = scanner.nextInt(); // duração brinquedo
 				pontuacao[w] = scanner.nextInt(); // pontuação brinquedo
+				for (int y = 0; y < t; y++)
+					aux[w][y] = -1;
 			}
 			System.out.println("Instancia " + r);
-			System.out.println(colecao(0, t));
+			System.out.println(colecao(0, t) + "\n");
 			r++;
 		}while(true);
 	}
