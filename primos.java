fc2b1dd6fb6d3815fcb86d0367bbbf432d5980b8
LeeonardoVargas <leu1607@hotmail.com>
2018-03-28 19:30:38 -0300

    primo

--- primos.java
+++ primos.java
@@ -2,38 +2,32 @@
 import java.util.*;
 import java.io.IOException;;
 class Main{
-	public static void main( String [] args ) throws IOException {
-		Scanner scanner = new Scanner(System.in);
-		int q = scanner.nextInt(), tam = 0;
+	public static int tam = 1000000;
+	public static long[] primo = new long[tam];
+	public static void main(String [] args) throws IOException {
+		Scanner ler = new Scanner(System.in);
+		int q = ler.nextInt();
+		primo[0] = 123;
+		for(int i = 2; i < tam-5; i++)
+			if(primo[i] != 123)
+				for(int j = i; i*j < tam-5; j++)
+					primo[j] = 123;
 		while(q != 0){
-			int x = scanner.nextInt(); // numero de brinquedos
-			int y = scanner.nextInt(); // tempo limite no parque
-			tam = y-x+2;
-			int[] numeros = new int[tam], gemeos = new int[tam]; // tempo limite no parque
-			ArrayList <Integer> primos = new ArrayList<Integer>();
-			// Set<Integer> gemeos = new HashSet<Integer>(); // set não permite add elementos repetidos
-			for(int i = x, k = 0; i <= y+1; i++, k++){
-				gemeos[k] = -1;
-				numeros[k] = i;
+			int x = ler.nextInt(), y = ler.nextInt(), maior = 0, menor = 0, gemeos = 0;
+			if(x < y){
+				maior = y;
+				menor = x;
+			} else {
+				maior = x;
+				menor = y;
 			}
-			int contador, numero = 0;
-			for (int i : numeros){
-				contador = 0;
-				for (int u = 1; u <= i; u++) {
-					if (i % u == 0){
-						contador++;
-						numero = u;
+			for(int n = menor; n <= maior; n++)
+				if(primo[n] != 123 && n > 1)
+					if(primo[n+2] != 123 || primo[n-2] != 123){
+						System.out.println("gemeo: " + n);
+						gemeos++;
 					}
-				}
-				if (contador == 2)
-					primos.add(numero);
-			}
-			contador = 0;
-			for (Integer i : primos)
-				for(int a = 0; a < primos.size(); a++)
-					if(Math.abs(primos.get(a) - i) == 2 && i <= y)
-						gemeos.add(i);
-			System.out.println(gemeos.size());
+			System.out.println(gemeos);
 			q--;
 		}
 	}
