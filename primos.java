a26c319ca3d49d66a460195695b92ffbdb94c710
LeeonardoVargas <leu1607@hotmail.com>
2018-04-13 13:15:03 -0300

    sadasd

--- primos.java
+++ primos.java
@@ -1,17 +1,24 @@
-// Problema da URI número 1487
+// Problema da URI número 1926
 import java.util.*;
 import java.io.IOException;;
 class Main{
-	public static int tam = 1000000;
-	public static long[] primo = new long[tam];
 	public static void main(String [] args) throws IOException {
 		Scanner ler = new Scanner(System.in);
 		int q = ler.nextInt();
+		int tam = 100;
+		long[] primo = new long[tam];
 		primo[0] = 123;
-		for(int i = 2; i < tam-5; i++)
+		primo[1] = 123;
+		for(int i = 2; i < tam; i++)
 			if(primo[i] != 123)
-				for(int j = i; i*j < tam-5; j++)
+				for(int j = i*i; j < tam; j+=i)
 					primo[j] = 123;
+		for(int a = 0; a < tam; a++){
+			if(primo[a] != 123)
+				System.out.print(a + " - ");
+			//else
+			//	System.out.print(a + " x-x ");
+		}
 		while(q != 0){
 			int x = ler.nextInt(), y = ler.nextInt(), maior = 0, menor = 0, gemeos = 0;
 			if(x < y){
@@ -23,10 +30,8 @@ class Main{
 			}
 			for(int n = menor; n <= maior; n++)
 				if(primo[n] != 123 && n > 1)
-					if(primo[n+2] != 123 || primo[n-2] != 123){
-						System.out.println("gemeo: " + n);
+					if(primo[n+2] != 123 || primo[n-2] != 123)
 						gemeos++;
-					}
 			System.out.println(gemeos);
 			q--;
 		}
